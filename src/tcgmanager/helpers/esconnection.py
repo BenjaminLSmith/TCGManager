ed1d73aea3cecfbe7e2dc99fe03dd20d0c375673
Benjamin Smith <bsmithjunior@gmail.com>
2019-07-06 11:06:32 -0500

    formatting fixes

--- src/tcgmanager/helpers/esconnection.py
+++ src/tcgmanager/helpers/esconnection.py
@@ -1,19 +1,41 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
 
 import logging
 
 LOG = logging.getLogger(name=__name__)
 
+
 class ESConnectionBase:
     def __init__(self, esHost):
         self.conn = Elasticsearch([esHost])
+        self.invIndex = "mtg-inventory"
+        self.priceIndex = "mtg-prices"
 
-
-    def insertDocument(self, document):
-        # Insert time into document
-        document['timestamp'] = datetime.utcnow()
+    def insertInvDocument(self, document):
+        # create Document ID
+        docID = "{}-{}".format(
+            document["productId"], document["subTypeName"]
+        )
 
         # Insert document into ES
-        res = self.conn.index(index="mtg-cards", body=document)
+        res = self.conn.index(
+            index=self.invIndex, body=document, id=docID, doc_type="cards"
+        )
         return res
+
+    def getInvList(self):
+
+        res = self.conn.search(
+        )
+
+        return res
+
+    def getCardByDocID(self, docID):
+        try:
+            res = self.conn.get(index=self.invIndex, doc_type='cards', id=docID)
+        except NotFoundError:
+            return None
+        finally:
+            return res["_source"]
--- src/tcgmanager/helpers/tcgplayer.py
+++ src/tcgmanager/helpers/tcgplayer.py
@@ -8,7 +8,8 @@ from urllib.parse import urlencode
 
 LOG = logging.getLogger(name=__name__)
 
-class TCGPlayerBase():
+
+class TCGPlayerBase:
     def __init__(self):
         self.authCode = self.getAuthCode()
         self.host = "http://api.tcgplayer.com/v1.27.0"
@@ -23,7 +24,9 @@ class TCGPlayerBase():
 
             # Check if auth code is still valid
             now = datetime.now()
-            expires = datetime.strptime(authcodeJSON[".expires"], "%a, %d %b %Y %H:%M:%S GMT")
+            expires = datetime.strptime(
+                authcodeJSON[".expires"], "%a, %d %b %Y %H:%M:%S GMT"
+            )
             if now > expires:
                 return self.authenticate()
             else:
@@ -38,29 +41,31 @@ class TCGPlayerBase():
 
         with open("secrets/key.priv", "r") as f:
             private_key = f.readline()
-        url = 'https://api.tcgplayer.com/token'
+        url = "https://api.tcgplayer.com/token"
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
-        body = "grant_type=client_credentials&client_id={}&client_secret={}".format(public_key.strip(), private_key.strip())
+        body = "grant_type=client_credentials&client_id={}&client_secret={}".format(
+            public_key.strip(), private_key.strip()
+        )
         r = requests.post(url, data=body, headers=headers)
         # Write new auth code to file & return code
         resp = json.loads(r.text)
         with open("secrets/authcode", "w+") as f:
             f.write(r.text)
 
-        return resp['access_token']
+        return resp["access_token"]
 
     # Returns a JSON array of the categories available
     def listCategories(self):
         endpoint = "/catalog/categories"
         url = self.host + endpoint
 
-        headers = { "Authorization": "bearer {}".format(self.authCode)}
+        headers = {"Authorization": "bearer {}".format(self.authCode)}
 
         results = json.loads(requests.get(url, headers=headers).text)
         maxResults = results["totalItems"]
         currResults = results["results"]
-        while(len(currResults) < maxResults):
+        while len(currResults) < maxResults:
             newUrl = url + "?offset={}".format(len(currResults))
             pagResults = json.loads(requests.get(newUrl, headers=headers).text)
             currResults.extend(pagResults["results"])
@@ -70,11 +75,8 @@ class TCGPlayerBase():
     # Returns a results response searching by card name
     def mtgCardSearch(self, name=None):
         endpoint = "/catalog/products"
-        headers = { "Authorization": "bearer {}".format(self.authCode)}
-        params = {
-            "categoryId": 1,
-            "productTypes": "Cards"
-        }
+        headers = {"Authorization": "bearer {}".format(self.authCode)}
+        params = {"categoryId": 1, "productTypes": "Cards"}
 
         params["productName"] = name
 
@@ -85,13 +87,10 @@ class TCGPlayerBase():
 
     # Gets card details with a list of product IDs
     def mtgCardDetails(self, productIds):
-        params = {
-            "includeSkus": False,
-            "getExtendedFields": True
-        }
+        params = {"includeSkus": False, "getExtendedFields": True}
         endpoint = "/catalog/products/{}".format(",".join(str(x) for x in productIds))
         url = "{}{}".format(self.host, endpoint)
-        headers = { "Authorization": "bearer {}".format(self.authCode)}
+        headers = {"Authorization": "bearer {}".format(self.authCode)}
         results = json.loads(requests.get(url, headers=headers, params=params).text)
 
         LOG.debug(json.dumps(results, indent=4, sort_keys=True))
@@ -100,7 +99,7 @@ class TCGPlayerBase():
     def getMarketPricesByProductId(self, productIds):
         endpoint = "/pricing/product/{}".format(",".join(str(x) for x in productIds))
         url = "{}{}".format(self.host, endpoint)
-        headers = { "Authorization": "bearer {}".format(self.authCode)}
+        headers = {"Authorization": "bearer {}".format(self.authCode)}
 
         results = json.loads(requests.get(url, headers=headers).text)
 
